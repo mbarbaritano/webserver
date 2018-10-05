c5b07ef5f06f78c2eb848bf4207cf7a180ee927b
mbarbaritano <mbarbaritano@deloitte.com.au>
2018-10-05 04:50:15 +0000

    website

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,37 @@
 class webserver {
     #this modules will manage my web server
 
-file    {"c:\\Initpub\\website":
+file    { "c:\\Inetpub\\website":
 ensure => directory,
 }
 
-file {"c:\\Initepub\\website\\index.html":
+file { "index":
 ensure => file,
+path   => "c:\\Inetpub\\website\\index.html",
 source => 'puppet:///modules/webserver/index.html',
+}
+
+iis_features = ['Web-WebServer','Web-Scripting-Tools']
+
+iis_feature { $iis_features:
+  ensure => 'present',
+}
+
+# Delete the default website to prevent a port binding conflict.
+iis_site {'Default Web Site':
+  ensure  => absent,
+  require => Iis_feature['Web-WebServer'],
+}
+
+iis_site { 'website':
+  ensure          => 'started',
+  physicalpath    => 'c:\\inetpub\\website',
+  applicationpool => 'DefaultAppPool',
+  require         => [
+    File["index"],
+    Iis_site['Default Web Site']
+  ],
+}
+
+
 }
