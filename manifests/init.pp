4f33d08c95e2965c96b448c1184200124e80f982
mbarbaritano <mbarbaritano@deloitte.com.au>
2018-10-05 04:12:57 +0000

    ppiles

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,11 @@
 class webserver {
     #this modules will manage my web server
 
-    
+file    {"c:\\Initpub\\website":
+ensure => directory,
+}
+
+file {"c:\\Initepub\\website\\index.html":
+ensure => file,
+source => 'puppet:///modules/webserver/index.html',
 }
